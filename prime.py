fa4248b22f274750d4c6a92ef90e9f0982193a43
dagudu123 <gayatridagudu@gmail.com>
2024-11-27 17:01:19 -0800

    Update prime.py

--- prime.py
+++ prime.py
@@ -5,26 +5,26 @@ primes = [2]
 
 n=3
 range = 2
-count = 9999
+count = 999
 start = time.time()
 while count > 0:
-        #Efficient way to compute range instead of sqrt
-        if range * range > n:
-                range += 1
+	#Efficient way to compute range instead of sqrt
+	if range * range > n:
+		range += 1
+		
+	prime = True
+	for divisor in primes:
+		if n > range:
+			break
+		if n % divisor == 0:
+			prime = False
+			break
 
-        prime = True
-        for divisor in primes:
-                if n > range:
-                        break
-                if n % divisor == 0:
-                        prime = False
-                        break
-
-        if (prime):
-                primes.append(n)
-                count -= 1
+	if (prime):
+		primes.append(n)
+		count -= 1
         
-        n += 1
+	n += 1
 end = time.time()
-# print (primes)
+print (primes)
 print(end-start)
